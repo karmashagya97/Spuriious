7ef1037fa9a58bdd3f0adadf0e2401e0cc66a4e6
karmashagya <karmashagya44@gmail.com>
2020-06-20 12:57:42 +0530

    Update Fake_news_detection.py

--- Fake_news_detection.py
+++ Fake_news_detection.py
@@ -18,7 +18,7 @@ y = news['label']
 #Splitting the data into train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
 
-#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
+#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Linear SVC model
 pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
                     ('svcmodel', LinearSVC())])
 
